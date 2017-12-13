6110d402599a1a6d456d3782a4f3721a5e9bdd89
JamesSmith1202 <jsmith@stuy.edu>
2017-12-13 14:44:08 -0500

    got the program working besides for the victory message

--- find.js
+++ find.js
@@ -1,11 +1,16 @@
+var hue = 165;
+var saturation = 100;
+var light = 57;
+
+var threshold = 5;
 var box = document.getElementById("box");
 var boxHeight = box.offsetHeight;
 var boxWidth = box.offsetWidth;
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
+var targetX = Math.floor(boxWidth * Math.random());
+var targetY = Math.floor(boxHeight * Math.random());
 
 
 console.log( "box height: " + boxHeight );
@@ -13,14 +18,14 @@ console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-    return (x0**2 -x1**2 + y0**2 - y1**2)**(1/2);
+    return ((x0-x1)**2 + (y0-y1)**2)**(1/2);
 };
 
 var findMaxDist = function(){
     var corners = [[0,0], [0, boxHeight], [boxWidth, 0], [boxWidth, boxHeight]];
     var maxDist = 0;
     var temp = 0;
-    for(int i = 0; i < 4; i++){
+    for(var i = 0; i < 4; i++){
 	temp = distance(targetX, targetY, corners[i][0], corners[i][1]);
 	if(temp > maxDist){
 	    maxDist = temp;
@@ -29,11 +34,18 @@ var findMaxDist = function(){
     return maxDist;
 };
 
+var maxDist = findMaxDist();
+
 var findIt = function(e) {
     var x = event.clientX;
     var y = event.clientY;
     var dist = distance(targetX, targetY, x, y);
-    box.style.color = rgb(255,200,183);
+    if (dist < threshold){
+	//you win
+    }
+    else{
+	box.style.backgroundColor = "hsl("+hue+","+saturation+"%,"+light*((maxDist-dist)/maxDist)+"%)";
+    }
 };
 box.addEventListener("mousemove", findIt);
 
